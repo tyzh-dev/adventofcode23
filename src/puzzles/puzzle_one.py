22044a6b3f2e0995cec4879e5e4cf6481819aaff
Tony <51972581+tony1658@users.noreply.github.com>
2023-12-01 12:55:42 +0100

    feat: Solve first puzzle

---
+++ src/puzzles/puzzle_one.py
@@ -0,0 +1,24 @@
+def get_digits(line):
+    digits = [int(char) for char in line if char.isdigit()]
+    return digits
+
+
+def get_calibration_value(digits):
+    first_digit = str(digits[0])
+    last_digit = str(digits[-1])
+    calibration_value = int(first_digit + last_digit)
+    return calibration_value
+
+
+def getSum():
+    with open("lib/input/input_day_one.txt") as file:
+        total = 0
+        for line in file:
+            digits = get_digits(line)
+            calibration_value = get_calibration_value(digits)
+            total = total + calibration_value
+        return total
+
+
+if __name__ == "__main__":
+    print(getSum())
